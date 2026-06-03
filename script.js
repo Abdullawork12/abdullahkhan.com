73b33468de72378888bda626a5bd1bfca9bca752
Abdullawork12 <abdullakhan.work@gmail.com>
2026-06-03 15:44:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function changeTheme() {
       ? "black"
       : "white";
 }
+function sendMessage() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  if (name === "" || email === "" || message === "") {
+    document.getElementById("status").innerText = "Please fill all fields ❌";
+    return;
+  }
+
+  document.getElementById("status").innerText =
+    "Thanks " + name + "! Message sent successfully ✅";
+
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
+}
