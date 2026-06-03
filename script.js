b656b098c9f2336ff1752868702ee3d9df02b072
Abdullawork12 <abdullakhan.work@gmail.com>
2026-06-03 15:22:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeTheme() {
+  document.body.style.background =
+    document.body.style.background === "black"
+      ? "#f2f2f2"
+      : "black";
+
+  document.body.style.color =
+    document.body.style.color === "white"
+      ? "black"
+      : "white";
+}
