83b55fdb414ccb31c181d6702f09007261d36350
Taxsphere <abdullakhan.work@gmail.com>
2026-06-04 00:30:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-document.getElementById("quoteForm").addEventListener("submit", function(e){
+document
+.getElementById("quoteForm")
+.addEventListener("submit", function(e){
 
 e.preventDefault();
 
-alert("Thank you for contacting Taxsphere. We will get back to you shortly.");
+alert(
+"Thank you for contacting Taxsphere. Our team will reach out shortly."
+);
 
 this.reset();
 
