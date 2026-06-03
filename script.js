9bf76af6383e46c6dacb7fa9cdda3adf28673e5a
Taxsphere <abdullakhan.work@gmail.com>
2026-06-03 23:02:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-document
-.getElementById("contactForm")
-.addEventListener("submit", function(e){
+// MOBILE MENU
+function toggleMenu(){
+document.querySelector("nav ul").classList.toggle("active");
+}
 
-    e.preventDefault();
+// COUNTER ANIMATION
+const counters = document.querySelectorAll(".counter");
 
-    alert(
-        "Thank you for contacting Taxsphere. We will contact you soon."
-    );
+counters.forEach(counter=>{
+const update=()=>{
+const target=+counter.getAttribute("data-target");
+const count=+counter.innerText;
+const inc=target/100;
 
-    this.reset();
+if(count<target){
+counter.innerText=Math.ceil(count+inc);
+setTimeout(update,20);
+}else{
+counter.innerText=target;
+}
+}
+update();
 });
